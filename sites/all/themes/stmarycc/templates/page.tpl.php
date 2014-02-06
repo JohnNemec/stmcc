a816a38002c50153c1e98de761cb67185d62bc27
John Nemec <johnnemec@gmail.com>
2014-02-06 13:13:37 -0600

    removed ckeditor module, added ckeditor to wysiwyg

--- sites/all/themes/stmarycc/templates/page.tpl.php
+++ sites/all/themes/stmarycc/templates/page.tpl.php
@@ -125,13 +125,15 @@
 
     <!-- region: Header -->
     <?php print render($page['header']); ?>
+  
+    <!-- Navigation elements -->
+    <?php print render($page['menu_bar']); ?>
+    <?php if ($secondary_navigation): print $secondary_navigation; endif; ?>
+    <?php if ($primary_navigation): print $primary_navigation; endif; ?>
+    
 
   </header>
 
-  <!-- Navigation elements -->
-  <?php print render($page['menu_bar']); ?>
-  <?php if ($primary_navigation): print $primary_navigation; endif; ?>
-  <?php if ($secondary_navigation): print $secondary_navigation; endif; ?>
 
   <!-- Breadcrumbs -->
   <?php if ($breadcrumb): print $breadcrumb; endif; ?>
